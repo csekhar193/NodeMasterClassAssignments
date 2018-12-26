a506a8f03b85790b1847db590037a6c0eb097088
csekhar193 <chandrasekhar193.ece@gmail.com>
2018-12-26 15:06:42 +0530

    removed the credentials.

--- homework#2/lib/config.js
+++ homework#2/lib/config.js
@@ -12,11 +12,11 @@ environmentVariable.development = {
 	'httpsPort' : 3001,
 	'envName' : 'development',
 	'hashingSecret' : 'ThisIsASecret', 
-	'stripeToken': 'sk_test_ZA7Im5RCM2nh5SiDYnbFIprb:',
+	'stripeToken': '',
 	'mailgun': {
-		'from': 'postmaster@sandbox22be83c3bb3d424bbc0c98be806cd62a.mailgun.org',
-		'domain': 'sandbox22be83c3bb3d424bbc0c98be806cd62a.mailgun.org', 
-		'token': '01d712e39ab778de17067b41ca75bf72-41a2adb4-ed2e0049'
+		'from': '',
+		'domain': '', 
+		'token': ''
 	}
 }
 
@@ -26,11 +26,11 @@ environmentVariable.production = {
 	'httpsPort' : 5001,
 	'envName' : 'production',
 	'hashingSecret' : 'ThisIsAlsoASecret',
-	'stripeToken': 'sk_test_ZA7Im5RCM2nh5SiDYnbFIprb:',
+	'stripeToken': '',
 	'mailgun': {
-		'from': 'postmaster@sandbox22be83c3bb3d424bbc0c98be806cd62a.mailgun.org',
-		'domain': 'sandbox22be83c3bb3d424bbc0c98be806cd62a.mailgun.org', 
-		'token': '01d712e39ab778de17067b41ca75bf72-41a2adb4-ed2e0049'
+		'from': '',
+		'domain': '', 
+		'token': ''
 	}
 }
 
